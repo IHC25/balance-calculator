971fa06ecd3a69f6baf553a0065160dbdf1fd867
arefin6 <imranhchy@gmail.com>
2022-02-17 11:54:45 +0600

    calculate button handled

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// enabling buttons when input is filled
 function enableButton(inputContainer, buttonName) {
   document
     .getElementById(inputContainer + "-container")
@@ -15,6 +14,38 @@ function enableButton(inputContainer, buttonName) {
       }
     });
 }
+function getInput(inputName) {
+  const inputText = document.getElementById(inputName + "-input");
+  const input = parseFloat(inputText.value);
+  return input;
+}
 
+function getExpenses(food, rent, clothes) {
+  const expenses = food + rent + clothes;
+  return expenses;
+}
+function getBalance(incomeAmount, expensesAmount) {
+  const balance = incomeAmount - expensesAmount;
+  return balance;
+}
+// enable disabled buttons when input is filled
 enableButton("calculate", "calculate");
 enableButton("save", "save");
+// calculate button handle
+document
+  .getElementById("calculate-button")
+  .addEventListener("click", function () {
+    const income = getInput("income");
+    const foodExpense = getInput("food");
+    const rentExpense = getInput("rent");
+    const clothesExpense = getInput("clothes");
+    const totalExpenses = document.getElementById("total-expenses");
+    const totalBalance = document.getElementById("total-balance");
+    // expense calculation
+    const expenses = getExpenses(foodExpense, rentExpense, clothesExpense);
+    // balance calculation
+    const balance = getBalance(income, expenses);
+    // show output
+    totalExpenses.innerText = expenses;
+    totalBalance.innerText = balance;
+  });
